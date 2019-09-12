988cc0b250b82899eda4a5322d83d08243761440
jaco7220 <jaco7220@ISS2V1SKH2.scdsb.pri>
2019-09-12 10:48:48 -0400

    Edit

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -1,44 +1,43 @@
 
 import TurtleGraphics.Pen;
+import TurtleGraphics.SketchPadWindow;
+import TurtleGraphics.StandardPen;
 import java.awt.Color;
 import java.util.Scanner;
 
 public class BarChart3 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int y=225;
+        Scanner s = new Scanner(System.in);
+        Pen p = new StandardPen(new SketchPadWindow(800, 600));
         Color col[] = {Color.red, Color.blue, Color.green, Color.orange};
-        Bar b[] = new PenBar[4];
-        for (int i = 0; i < b.length; i++) {
-            b[i] = new PenBar();
+        PenBar pb[] = new PenBar[4];
+        for (int i = 0; i < pb.length; i++) {
+            pb[i] = new PenBar();
+            pb[i].yloc=y;
+            y-=100;
             while (true) {
-                System.out.print("Enter height of bar " + (i + 1) + " (Must be between 0 and 450) > ");
-                b[i].height = scan.nextInt();
-                if (b[i].height > 1 && b[i].height <= 450) {
+                System.out.print("Enter height of bar " + (i + 1) + " (Must be between 0 and 750) > ");
+                pb[i].height = s.nextInt();
+                if (pb[i].height > 1 && pb[i].height <= 750) {
                     break;
                 } else {
                     System.out.println("Invalid please try again");
                 }
             }
+            makeBar(p, pb[i], pb[i].yloc, pb[i].height, col[i]);
+            pb[i].draw(p);
             
 
         }
-        
-        //draw the bars
-        b1.draw(c);
-        b2.draw(c);
-        b3.draw(c);
-        b4.draw(c);
-        c.setColor(Color.black);
-        c.drawString("Your graph", 300, 15);
 
     }
     
-    public static void makeBar(Pen p, PenBar pb, int x, int y, int h, Color col){
-        pb.x = x;
-        pb.y = y;
+    public static void makeBar(Pen p, PenBar pb, int y, int h, Color col){
+        pb.yloc = y;
         pb.height = h;
-        pb.barColor = col;
+        pb.col = col;
         pb.draw(p);
     }
 
--- src/PenBar.java
+++ src/PenBar.java
@@ -1,4 +1,22 @@
 
+import java.awt.Color;
+import TurtleGraphics.Pen;
+
+
 public class PenBar {
+    int yloc, height;
+    Color col;
     
+    public void draw(Pen p){
+        p.up();
+        p.setColor(col);
+        p.setWidth(50);
+        p.move(-400, yloc);
+        p.setDirection(0);
+        p.down();
+        p.move(height);
+        p.up();
+        p.move(30);
+        p.drawString("Value: " + height);
+    }
 }
